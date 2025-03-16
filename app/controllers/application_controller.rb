b2eedfd5bbcca70dc8c88c508456d4d6dc978276
Ryan Cusick <ryancusick@Ryans-MBP-2.local.tld>
2025-03-16 15:38:51 -0400

    Remove custom authenticate_user override in App controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@
   before_action :authenticate_user!, unless: :devise_controller?
 
   # Optional custom override (only if you need custom behavior)
-  def authenticate_user!
-    return if user_signed_in?
-    redirect_to new_user_session_path, alert: "Please sign in to access this page."
-  end
-end
+  #def authenticate_user!
+    #return if user_signed_in?
+    #redirect_to new_user_session_path, alert: "Please sign in to access this page."
+  #end
+#end
 
   def after_sign_in_path_for(resource)
     properties_path
@@ -15,4 +15,5 @@ end
  # Devise: Redirect after sign-out
   def after_sign_out_path_for(resource)
     root_path
+  end
   end
